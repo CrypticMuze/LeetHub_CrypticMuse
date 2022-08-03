4aeb5e27843fb820543d67c05cbea73f704f8104
Aman Anand <a.anand2k19@gmail.com>
2022-08-03 22:10:13 +0530

    Time: 2386 ms (5.03%), Space: 37.7 MB (87.88%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -0,0 +1,29 @@
+class MyCalendar {
+public:
+    vector<pair<int,int>>cal;
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        int n = cal.size();
+        sort(cal.begin(), cal.end());
+        if(cal.size()==0){
+            cal.push_back({start, end});
+            return true;
+        } else{
+            for(int i=0; i<n; i++){
+                if((start>=cal[i].second && end>=cal[i].second) || (start<=cal[i].first && end<=cal[i].first)) continue;
+                else return false;
+            }
+            cal.push_back({start, end});
+            return true;
+        }
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
